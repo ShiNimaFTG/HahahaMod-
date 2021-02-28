800d34a75c7f34cb251d3f0b935a9a7ee0160360
ShiNimaFTG <60620194+ShiNimaFTG@users.noreply.github.com>
2021-02-28 17:21:03 +0200

    Update HahahaMod.py

--- HahahaMod.py
+++ HahahaMod.py
@@ -5,19 +5,23 @@ def register(cb):
     cb(HahahaMod())
 
 class HahahaMod(loader.Module):
-	"""Хахахаталка от @OfficialShina. Использование: .ha <кол-во>"""
+	"""Хахахаталка от @OfficialShina. Использование: .ha <кол-во> (от 1 до 350) """
 	strings = {'name': 'Hahaha'}
 	async def hacmd(self, message):
 		args = utils.get_args_raw(message)
 		if args.isdigit():
-			haha = "ХА"*int(args)
-			reply = await message.get_reply_message() 
-			if reply:
-				await message.delete()
-				await reply.reply(haha)
+			if int(args) <= 350:
+				haha = "ХА"*int(args)
+				reply = await message.get_reply_message()
+				if reply:
+					await message.delete()
+					await reply.reply(haha)
+				else:
+					await message.delete()
+					await message.respond(haha)		 
 			else:
 				await message.delete()
-				await message.respond(haha)
+				await message.respond('ХА'*350)
 		else:
-			message.respond('Чел, ты....')
-			
+			await message.delete()
+			await message.respond('ХА')
